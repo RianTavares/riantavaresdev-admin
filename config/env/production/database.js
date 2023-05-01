296483ffc2292db099128a59886c7e77caac2987
Rian Tavares <rian.tavares@live.com>
2023-05-01 14:16:35 -0300

    first commit

---
+++ config/env/production/database.js
@@ -0,0 +1,16 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: config.PGHOST,
+        port: config.PGPORT,
+        database: config.PGDATABASE,
+        user: config.PGUSER,
+        password: config.PGPASSWORD,
+        ssl: env.bool(true),
+      },
+    },
+  });
